2e598f5e8511ec83484d0785934e9acfda80ddc0
MarylandGrown <paleagles93@yahoo.com>
2020-04-02 14:01:53 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {random } from 'lodash'
+import QuoteMachine from './components/QuoteMachine'
+import 'typeface-roboto';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+   constructor(props) {
+     super(props)
+   
+     this.state = {
+        quotes: [],
+        selectedQuoteIndex: null
+     }
+
+     this.assignedQuote = this.assignedQuote.bind(this)
+     this.choosenQuote = this.choosenQuote.bind(this);
+   }
+   
+   componentDidMount(){
+     fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+     .then(data => data.json())
+     .then(quotes=> this.setState({quotes}, this.assignedQuote))
+   }
+
+   get selectedQuote(){
+     if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)){
+       return undefined;
+     }
+     return this.state.quotes[this.state.selectedQuoteIndex];
+
+    }
+   
+
+choosenQuote(){
+  if(!this.state.quotes.length ){
+  return undefined;
+  }
+
+  // random is from lodash
+  return random(0, this.state.quotes.length-1)
+}
+
+assignedQuote(){
+  this.setState({selectedQuoteIndex: this.choosenQuote()})
+}
+
+  render() {
+    // console.log(this.state.quotes)
+    console.log(this.state.selectedQuoteIndex)
+
+    return (
+    
+        <div className="App">
+        
+          <div id="quote-box">
+            {
+              this.selectedQuote ?
+            <QuoteMachine selectedQuote={this.selectedQuote} assignedQuote={this.assignedQuote}/>
+            : null
+            }
+            
+          </div>
+      </div>
+
+         
+    )
+  }
 }
 
 export default App;
+
+
+
+
